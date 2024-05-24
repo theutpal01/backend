303566d61d07cf9241b3bee45fb54ef335c74927
Utpal <theutpal11@gmail.com>
2024-05-25 00:03:34 +0530

    INIT: cloudinary service

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,41 +1,43 @@
 import mongoose, { Schema, model } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new Schema({
-  videoFile: {
-    type: String,  // cloudnary
-    required: true,
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudnary
+      required: true,
+    },
+    thumbnail: {
+      type: String, // cloudnary
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // cloudnary
+      required: true,
+    },
+    view: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "",
+    },
   },
-  thumbnail: {
-    type: String,  // cloudnary
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  duration: {
-    type: Number,  // cloudnary
-    required: true,
-  },
-  view: {
-    type: Number,
-    default: 0,
-  },
-  isPublished: {
-    type: Boolean,
-    default: true,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: ""
-  }
-
-}, {timeseries: true});
+  { timeseries: true }
+);
 
 videoSchema.plugin(mongooseAggregatePaginate);
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        // upload to cloudinary
+        const res = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+
+        // file has been uploaded
+        console.log("File is uploaded on cloudinary", res.url)
+        return res;
+    } catch (err) {
+        fs.unlinkSync(localFilePath) // remove the locally save temp file as operation failed
+        return null;
+    }
+};
+
+export { uploadOnCloudinary };
