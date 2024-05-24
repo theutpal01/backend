779da566aae3ed20cafe6615e6de34c3e735a344
Utpal <theutpal11@gmail.com>
2024-05-24 17:29:10 +0530

    INIT: jwt, bcrypt and tokens

---
+++ src/models/video.models.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema, model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+  videoFile: {
+    type: String,  // cloudnary
+    required: true,
+  },
+  thumbnail: {
+    type: String,  // cloudnary
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,  // cloudnary
+    required: true,
+  },
+  view: {
+    type: Number,
+    default: 0,
+  },
+  isPublished: {
+    type: Boolean,
+    default: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: ""
+  }
+
+}, {timeseries: true});
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = model("Video", videoSchema);
