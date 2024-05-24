78923712c53b72ae88e0889b1ddd757255f95ee5
Utpal <theutpal11@gmail.com>
2024-05-24 12:22:20 +0530

    INIT: mongo database

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const conInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`Connected to MongoDB! DB HOST: ${conInstance.connection.host}`);
+    } catch (err) {
+        console.log("MONGO AUTH_ERR:", err);
+        process.exit(1);
+    }
+};
+
+
+export default connectDB;
